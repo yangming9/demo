bc9918c2146da25cdddf2a6cf14136d0f4466ede
yangming <yangming21_ok@163.com>
2020-06-01 17:27:24 +0800

    init project

---
+++ src/test/java/com/yangming/boot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangming.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
